6d7654edd42f1bbf0f4725f7b426e7a83fd1895b
Gui <Gui@host.docker.internal>
2025-01-19 19:53:02 -0300

    initial

---
+++ springsecurity/src/test/java/guicerqueir4/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package guicerqueir4.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
